6457b64712284ac3893b687126443766bae6a816
Mona Raj <agrmonaraj@gmail.com>
2025-11-11 11:54:38 +0530

    Add overlay on hovering above images in gallery

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const galleryContainer = document.getElementById("gallery-container");
 
 gallery.forEach((category) => {
   const section = document.createElement("section");
+  section.setAttribute("id", category.heading.toLowerCase());
 
   section.innerHTML = `
     <h2>${category.heading}</h2>
@@ -13,6 +14,10 @@ gallery.forEach((category) => {
         return `
         <div class="img-card">
           <img src="${image.filename}" alt="${image.title}" />
+          <div class="img-card-overlay">
+            <h3>${image.title}</h3>
+            <button class="heart-btn">❤</button>
+          </div>
         </div>
       `;
       })

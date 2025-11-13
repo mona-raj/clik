eb012f23b0498e49c4dedfbe4d0dfae3421323a5
Mona Raj <agrmonaraj@gmail.com>
2025-11-13 18:48:02 +0530

    Apply popup logic to all images rendered through js

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
-// import { gallery } from "./data/gallery.js";
+import { gallery } from "./data/gallery.js";
 
-// const galleryContainer = document.getElementById("gallery-container");
+function renderGallery() {
+  const galleryContainer = document.getElementById("gallery-container");
 
-// gallery.forEach((category) => {
-//   const section = document.createElement("section");
-//   section.setAttribute("id", category.heading.toLowerCase());
+  gallery.forEach((category) => {
+    const section = document.createElement("section");
+    section.setAttribute("id", category.heading.toLowerCase());
 
-//   section.innerHTML = `
-//     <h2>${category.heading}</h2>
-//     <div class="category-container">
-//     ${category.images
-//       .map((image) => {
-//         return `
-//         <div class="img-card">
-//           <img src="${image.filename}" alt="${image.title}" />
-//           <div class="img-card-overlay">
-//             <h3>${image.title}</h3>
-//             <button class="heart-btn">❤</button>
-//           </div>
-//         </div>
-//       `;
-//       })
-//       .join("")}
-//     </div>
-//   `;
-//   galleryContainer.appendChild(section);
-// });
+    section.innerHTML = `
+    <h2>${category.heading}</h2>
+    <div class="category-container">
+    ${category.images
+      .map((image, index) => {
+        return `
+        <div class="img-card">
+            <img src=${image.filename} alt=${image.title} data-category="${
+          category.heading
+        }" data-index="${index}"/>
+            <div class="img-card-overlay">
+              <h3>${image.title}</h3>
+              <button class="heart-btn">❤</button>
+            </div>
+
+            <div class="img-popup" data-category="${
+              category.heading
+            }" data-index="${index}">
+              <div>
+                <img src=${image.filename} alt=${image.title} />
+              </div>
+              <div class="img-popup-details">
+                <h2>${image.title}</h2>
+                <p>
+                  ${image.description}
+                </p>
+                <div class="img-tag-container">
+                  <img src="../icons/tags.svg" alt="Tags">
+                  ${image.tags
+                    .map((tag) => {
+                      return `
+                    <span class="img-tag">${tag}</span>
+                    `;
+                    })
+                    .join("")}
+                </div>
+                <div class="img-popup-like-container">
+                  <button class="heart-btn active">❤</button>
+                  <span>${image.likes} likes</span>
+                </div>
+              </div>
+              <button class="img-popup-close-btn" data-category="${
+                category.heading
+              }" data-index="${index}">
+                <img src="../icons/close.svg" alt="">
+              </button>
+            </div>
+          </div>
+      `;
+      })
+      .join("")}
+    </div>
+  `;
+    galleryContainer.appendChild(section);
+  });
+}
 
 function handlePopup() {
   const imgCards = document.querySelectorAll(".img-card img");
@@ -58,4 +95,5 @@ function handlePopup() {
   });
 }
 
+renderGallery();
 handlePopup();
